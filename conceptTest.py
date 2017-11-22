857980454d9d16f278919da9a3313ef22e9d8716
Christian Dobias <32696880+CDobias14@users.noreply.github.com>
2017-11-21 17:49:30 -0800

    Update conceptTest.py

--- conceptTest.py
+++ conceptTest.py
@@ -2,27 +2,13 @@ import maya.cmds as cmds
 import maya.api.OpenMaya as om
 import math
 
-# Get world position of LOC 1
-wst = cmds.xform('shoulder_R_jointPosition', q=True, ws=True, t=True)
+# Get vectors of LOC 1 and LOC 2
+vec1 = cmds.xform('shoulder_R_jointPosition', q=True, ws=True, t=True)
 
-wst
+vec2 = cmds.xform('elbow_R_jointPosition', q=True, ws=True, t=True)
 
-# Get vector from LOC 1 to LOC 2
-vec1Raw = cmds.xform('shoulder_R_jointPosition', q=True, ws=True, t=True)
-vec1Raw
-#vec1Pos = om.MVector(vec1Raw[0], vec1Raw[1], vec1Raw[2])
-#vec1Pos
+# Get the angle of the vector that points from LOC 1 to LOC 2
+orient = cmds.angleBetween(er=True, v1=vec2, v2=vec1)
 
-vec2Raw = cmds.xform('elbow_R_jointPosition', q=True, ws=True, t=True)
-vec2Raw
-#vec2Pos = om.MVector(vec2Raw[0], vec2Raw[1], vec2Raw[2])
-#vec2Pos
-
-vecTotal = (vec2Pos - vec1Pos)
-
-# 
-orient = cmds.angleBetween(er=True, v1=vec2Raw, v2=vec1Raw)
-
-print(orient)
-
-cmds.xform('shoulder_R_jointPosition', a=True, ro=orient,)
+# Test for correct angle by orienting LOC 1
+cmds.xform('shoulder_R_jointPosition', a=True, ro=orient)
